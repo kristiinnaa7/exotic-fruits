f4f3f6242deb070ac31223dcfdf91d06b1f20fa2
Kristina <kristiinnaa7@gmail.com>
2024-11-24 16:44:27 +0200

    project configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyDLd_E68LOhdpixlKqpsK4rFpdYrWnlRNo",
+        authDomain: "exotic-fruits-d43be.firebaseapp.com",
+        databaseURL: "https://exotic-fruits-d43be-default-rtdb.firebaseio.com",
+        projectId: "exotic-fruits-d43be",
+        storageBucket: "exotic-fruits-d43be.firebasestorage.app",
+        messagingSenderId: "127733801190",
+        appId: "1:127733801190:web:4bf26fcdcb78c63e81db27",
+        measurementId: "G-V1TK88NRJN"
+      }
+}
